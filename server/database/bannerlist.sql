ab789c78f1ab89daab8ed66bf367f71b51850a1c
web_hello <web_hello@sina.com>
2021-01-14 19:54:10 +0800

    modefied: export database

---
+++ server/database/bannerlist.sql
@@ -0,0 +1,44 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : webxiaodudu
+ Source Server Type    : MySQL
+ Source Server Version : 50645
+ Source Host           : localhost:3306
+ Source Schema         : game_mod
+
+ Target Server Type    : MySQL
+ Target Server Version : 50645
+ File Encoding         : 65001
+
+ Date: 14/01/2021 19:48:34
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bannerlist
+-- ----------------------------
+DROP TABLE IF EXISTS `bannerlist`;
+CREATE TABLE `bannerlist`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `img` longtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `sub_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `info` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `logo` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of bannerlist
+-- ----------------------------
+INSERT INTO `bannerlist` VALUES (1, '10001', 'bg-epic-battles.jpg', '罗马全战', '罗马全战的小标题', '这是一个测试的数据', '');
+INSERT INTO `bannerlist` VALUES (2, '10002', '0159.jpg', '王牌军抗美援朝', '雄赳赳气昂昂，跨过鸭绿江！', '今日开始出售', NULL);
+INSERT INTO `bannerlist` VALUES (3, '10003', '0082.jpg', '大秦赋', '开启统一六国的征途！', '即将公测', NULL);
+INSERT INTO `bannerlist` VALUES (4, '1003', '0149.jpg', '大明抗倭', '回归大明，扫清倭寇！', '独立的将军模型，可diy定制', '');
+INSERT INTO `bannerlist` VALUES (5, '10004', '0199.jpg', '水浒英雄传', '大河向东流哇，你有我有他也有啊', '自制水浒英雄', '');
+
+SET FOREIGN_KEY_CHECKS = 1;
--- server/database/pchotlist.sql
+++ server/database/pchotlist.sql
@@ -11,35 +11,11 @@
  Target Server Version : 50645
  File Encoding         : 65001
 
- Date: 14/01/2021 15:07:37
+ Date: 14/01/2021 19:48:42
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for bannerlist
--- ----------------------------
-DROP TABLE IF EXISTS `bannerlist`;
-CREATE TABLE `bannerlist`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pid` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `img` longtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `sub_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `info` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `logo` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
-
--- ----------------------------
--- Records of bannerlist
--- ----------------------------
-INSERT INTO `bannerlist` VALUES (1, '10001', 'bg-epic-battles.jpg', '罗马全战', '罗马全战的小标题', '这是一个测试的数据', '');
-INSERT INTO `bannerlist` VALUES (2, '10002', '0159.jpg', '王牌军抗美援朝', '雄赳赳气昂昂，跨过鸭绿江！', '今日开始出售', NULL);
-INSERT INTO `bannerlist` VALUES (3, '10003', '0082.jpg', '大秦赋', '开启统一六国的征途！', '即将公测', NULL);
-INSERT INTO `bannerlist` VALUES (4, '1003', '0149.jpg', '大明抗倭', '回归大明，扫清倭寇！', '独立的将军模型，可diy定制', '');
-INSERT INTO `bannerlist` VALUES (5, '10004', '0199.jpg', '水浒英雄传', '大河向东流哇，你有我有他也有啊', '自制水浒英雄', '');
 
 -- ----------------------------
 -- Table structure for pchotlist
@@ -65,4 +41,11 @@ INSERT INTO `pchotlist` VALUES (1, '10001', 'origin_202101071552231242.jpg', '5'
 INSERT INTO `pchotlist` VALUES (2, '10002', '0.jpg', '3', '反恐起源', 'cs射击竞赛', 'EAGame', '1999-02-11', '3');
 INSERT INTO `pchotlist` VALUES (3, '10003', '8.jpg', '2', '测试数剧', '啥啥哈哈哈', '我的公司', '2005-01-01', '4');
 INSERT INTO `pchotlist` VALUES (4, '10004', '2.jpg', '1', '就叫忍者吧', '随便写点啥Asdasd按说的地方搞搞撒大声地', '二狗子公司', '2021-12-12', '2');
-INSERT INTO `pchotlist` VALUES (5, '10005', '7.jpg', '4', '幕府将军2', 'SAGA出品的第二代幕府全面战争，已日本战国为背景的题材战略游�
+INSERT INTO `pchotlist` VALUES (5, '10005', '7.jpg', '4', '幕府将军2', 'SAGA出品的第二代幕府全面战争，已日本战国为背景的题材战略游戏', 'SAGA', '2005-02-11', '2');
+INSERT INTO `pchotlist` VALUES (6, '10006', '4.jpg', '3', '蝙蝠侠', '嚓冠状病毒就你丫闹得！！', '冠状病毒公司嚓', '2020-01-22', '3');
+INSERT INTO `pchotlist` VALUES (7, '10007', '5.jpg', '5', '指环王吧', '破公司出品的一个什么玩意儿', '破公司', '1998-09-06', '3');
+INSERT INTO `pchotlist` VALUES (8, '10008', '3.jpg', '4', '忘记这个游戏是啥了', '好像是个打怪的游戏角色扮演的', '小日本的一个游戏公司', '1996-08-12', '3');
+INSERT INTO `pchotlist` VALUES (9, '10009', '1.jpg', '3', '孤岛惊魂', '一个第一人称射击的游戏，但我没玩过', '豆豆游戏公司', '2000-12-03', '2');
+INSERT INTO `pchotlist` VALUES (10, '10010', '6.jpg', '5', '图片小哥挺酷', '随便网上找的游戏图片', 'MY_NIUBI', '2010-05-01', '1');
+
+SET FOREIGN_KEY_CHECKS = 1;
